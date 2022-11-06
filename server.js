e3b338331c0e17ea03dd156abd6acb1fa81a15c1
BenedictBCJJ <bennyyxz@gmail.com>
2022-11-06 16:28:50 +0800

    Add redis for querying

--- server.js
+++ server.js
@@ -9,31 +9,53 @@ const port = process.env.PORT || 4000;
 let redisClient;
 
 (async () => {
-  redisClient = redis.createClient();
+  redisClient = redis.createClient({legacyMode: true});
 
   redisClient.on("error", (error) => console.error(`Error : ${error}`));
 
-  await redisClient.connect();
+  await redisClient.connect().then(console.log("redis connected"));
 })();
+
+function redisGet(res) {
+  return new Promise((resolve, reject) => {
+    redisClient.get("user", (err, reply) => {
+      if(err) {
+        console.log(err);
+      } else if (reply) {
+        const parsedJson = JSON.parse(reply);
+        res.json({
+          storageused: "redis",
+          status: "success",
+          message: "Contacts retrieved successfully",
+          data: parsedJson,
+        })
+      } else {
+        User.get(function (err, user) {
+          console.log("db used");
+          if (err) {
+            res.json({
+              status: "error",
+              message: err,
+            });
+          } else {
+            redisClient.set("user", JSON.stringify(user));
+            res.json({
+              storageused: "mongodb",
+              status: "success",
+              message: "Contacts retrieved successfully",
+              data: user,
+            });
+          }
+        });
+      }
+    })
+  });
+}
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
-app.get("/getall", (req, res) => {
-  User.get(function (err, user) {
-    // console.log(user);
-    if (err) {
-      res.json({
-        status: "error",
-        message: err,
-      });
-    } else {
-      res.json({
-        status: "success",
-        message: "Contacts retrieved successfully",
-        data: user,
-      });
-    }
-  });
+app.get("/getall", async (req, res) => {
+  await redisGet(res);
 });
